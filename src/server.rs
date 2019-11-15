a68fcbb949b13bc65eac646c80a530edca37f488
Matt Bell <mappum@gmail.com>
2019-11-15 12:24:00 -0800

    Made example implementation a little more advanced

--- src/server.rs
+++ src/server.rs
@@ -21,15 +21,34 @@ impl Server {
 
 #[cfg(test)]
 mod tests {
+    use crate::messages::abci;
     use super::*;
 
     #[test]
     fn server() {
-        fn handle_connection(i: usize, conn: Connection) -> ! {
-            loop {
+        fn handle_connection(i: usize, conn: Connection) {
+            std::thread::spawn(move || loop {
                 let req = conn.read();
                 println!("got request on connection {}: {:?}", i, req);
-            }
+
+                match req.value {
+                    Some(abci::Request_oneof_value::info(_)) => {
+                        let mut res = abci::Response::new();
+                        let mut info = abci::ResponseInfo::new();
+                        info.set_last_block_app_hash(vec![0; 20]);
+                        res.set_info(info);
+                        conn.write(res).unwrap();
+                    },
+
+                    Some(abci::Request_oneof_value::flush(_)) => {
+                        let mut res = abci::Response::new();
+                        res.set_flush(abci::ResponseFlush::new());
+                        conn.write(res).unwrap();
+                    },
+
+                    _ => {}
+                }
+            });
         }
 
         let server = Server::listen("localhost:26658").unwrap();
@@ -37,5 +56,6 @@ mod tests {
         handle_connection(1, server.accept().unwrap());
         handle_connection(2, server.accept().unwrap());
         handle_connection(3, server.accept().unwrap());
+        std::thread::park();
     }
 }
