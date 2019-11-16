281346368c034718e7bdd539c92f43002859a28d
Matt Bell <mappum@gmail.com>
2019-11-15 16:58:50 -0800

    Handle more messages in example

--- src/server.rs
+++ src/server.rs
@@ -30,27 +30,25 @@ mod tests {
     #[test]
     fn server() {
         fn handle_connection(i: usize, conn: Connection) {
+            use abci::Request_oneof_value::*;
+
             std::thread::spawn(move || loop {
                 let req = conn.read().unwrap();
                 println!("got request on connection {}: {:?}", i, req);
 
+                let mut res = abci::Response::new();
+
                 match req.value {
-                    Some(abci::Request_oneof_value::info(_)) => {
-                        let mut res = abci::Response::new();
-                        let mut info = abci::ResponseInfo::new();
-                        info.set_last_block_app_hash(vec![0; 20]);
-                        res.set_info(info);
-                        conn.write(res).unwrap();
-                    },
-
-                    Some(abci::Request_oneof_value::flush(_)) => {
-                        let mut res = abci::Response::new();
-                        res.set_flush(abci::ResponseFlush::new());
-                        conn.write(res).unwrap();
-                    },
-
+                    Some(info(_)) => res.set_info(abci::ResponseInfo::new());
+                    Some(init_chain(_)) => res.set_init_chain(abci::ResponseInitChain::new());
+                    Some(begin_block(_)) => res.set_begin_block(abci::ResponseBeginBlock::new());
+                    Some(end_block(_)) => res.set_end_block(abci::ResponseEndBlock::new());
+                    Some(commit(_)) => res.set_commit(abci::ResponseCommit::new());
+                    Some(flush(_)) => res.set_flush(abci::ResponseFlush::new());
                     _ => {}
                 }
+
+                conn.write(res).unwrap();
             });
         }
 
