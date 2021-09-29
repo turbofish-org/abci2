32e2d56cb6869694299c86170164ee675f3da507
Charlie Little <cwlittle@utexas.edu>
2021-09-29 14:17:39 -0500

    Add Error variants

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,23 @@
-pub use failure::Error;
+use std::sync::mpsc::RecvError;
+use std::sync::mpsc::SendError;
+pub use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    TendermintSend(#[from] SendError<tendermint_proto::abci::Response>),
+    #[error(transparent)]
+    ProstEncode(#[from] prost::EncodeError),
+    #[error(transparent)]
+    ProstDecode(#[from] prost::DecodeError),
+    #[error(transparent)]
+    RecV(#[from] RecvError),
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+    #[error("Request Error: {0}")]
+    Request(String),
+    #[error("Unknown Error")]
+    Unknown,
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
